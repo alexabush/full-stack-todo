fbec474140d1ca58b9f88ee1dafaa7ab68c1f963
Alex Bush <alexabush7@gmail.com>
2018-12-06 15:09:21 -0800

    made some little changes to see if I can get heroku app to run

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// app.get('/', (req,res) =>{
+//     res.sendFile(path.join(__dirname+'/client/build/index.html'));
+// });
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
