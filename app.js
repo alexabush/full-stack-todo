dc0d90a3816bd291ddd574263a8f04b6e8d4406d
Alex Bush <alexabush7@gmail.com>
2018-12-06 12:43:10 -0800

    made changes to package.json

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
